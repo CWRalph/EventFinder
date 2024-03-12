cb79b0b5d8bea543ea1404ed2b7034dd0c8ab20b
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2024-03-12 11:19:06 -0700

    added leaflet

---
+++ frontend/src/app/map-component/map.component.ts
@@ -0,0 +1,59 @@
+import {Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
+import {icon, Marker} from 'leaflet';
+
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+Marker.prototype.options.icon = iconDefault;
+
+@Component({
+  selector: 'map-component',
+  templateUrl: 'map.component.html',
+  standalone: true,
+  styleUrl: 'map.component.css'
+})
+
+export class MapComponent implements OnInit {
+  private map!: L.Map;
+  private markers: L.Marker[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.initializeMap();
+  }
+
+  private initializeMap(): void {
+    this.map = L.map('mapId').setView([49.2, -123], 11);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>' +
+          'contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+      }
+    ).addTo(this.map);
+  }
+
+  private clearMarkers(): void {
+    this.markers.forEach(marker => {
+      this.map.removeLayer(marker);
+    });
+    this.markers = [];
+  }
+
+}
