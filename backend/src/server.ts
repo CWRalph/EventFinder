ee5635b324dc2bcac3b9400871ca4706bb686dcd
TJ Grewal <tjgrewal.dev@gmail.com>
2024-03-20 00:23:06 -0700

    backend hotfix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -51,15 +51,4 @@ app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
 
-// Added functions for graceful exits
-mongoose.connection.on('error', (err) => {
-    console.log(err.message)
-});
-mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose connection is disconnected.')
-});
-process.on('SIGINT', async () => {
-    await mongoose.connection.close()
-    process.exit(0)
-});
 
