039fcf4e7950e67a8f7a579e8e882aa6474582b7
sepehr455 <84247783+sepehr455@users.noreply.github.com>
2024-04-03 14:04:29 -0700

    implemented notification for creating, accepting and rejecting friendships

--- backend/src/routes/friendship.ts
+++ backend/src/routes/friendship.ts
@@ -9,9 +9,38 @@ const verify = require('./verifyToken');
 
 const friendshipRouter = express.Router();
 
+const hitNotifier = async (user1Id: string, user2Id: string, url: string, res: express.Response) => {
+    const user1 = await User.findById(user1Id);
+    const user2 = await User.findById(user2Id);
+
+    if (!user1 || !user2) {
+        return notFound(res, 'One of both users');
+    }
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            userEmail: user1.email,
+            friend: user2.username,
+        }),
+    });
+}
+
+friendshipRouter.get('/', async (req, res) => {
+    try {
+        const friendships = await Friendship.find();
+        res.json(friendships);
+    } catch (e) {
+        catchError(e, res);
+    }
+});
+
 // This will deny access if user isn't verified
 // friendshipRouter.post('/', verify , async (req, res) => {
-friendshipRouter.post('/' , async (req, res) => {
+friendshipRouter.post('/', async (req, res) => {
     const {user1: user1Id, user2: user2Id, status} = req.body;
 
     try {
@@ -26,6 +55,17 @@ friendshipRouter.post('/' , async (req, res) => {
         await newFriendship.save();
 
         res.status(201).json(newFriendship);
+
+        // Hit the notifier endpoint to send an email
+        /**
+         * NOTE: we are assuming that user1 is the one receiving the friend request and user2 is the one sending it
+         */
+        await hitNotifier(user1Id, user2Id, 'http://localhost:4000/friendship/request', res).then(() => {
+            console.log('Email sent');
+        }, (e: Error) => {
+            catchError(e, res);
+        });
+
     } catch (e) {
         catchError(e, res);
     }
@@ -65,16 +105,53 @@ friendshipRouter.put('/:id/update-status', async (req, res) => {
     const {id} = req.params;
     const {status} = req.body;
 
+    const friendship = await Friendship.findByIdAndUpdate(id, {status}, {new: true});
+
+    if (!friendship) {
+        return notFound(res, 'Friendship');
+    }
+
+    const user1 = await User.findById(friendship.user1);
+    const user2 = await User.findById(friendship.user2);
+
+    if (!user1 || !user2) {
+        return notFound(res, 'One of both users');
+    }
+
     try {
-        const friendship = await Friendship.findById(id);
-        if (!friendship) {
-            return notFound(res, 'Friendship');
+
+        if (status == 'Accepted') {
+            if (!friendship) {
+                return notFound(res, 'Friendship');
+            }
+
+            // update the friendship status
+            friendship.status = status;
+            await friendship.save().then(() => {
+                console.log('Friendship updated');
+            });
+
+            res.json(friendship);
+
+            hitNotifier(friendship.user2.toString(), friendship.user1.toString(), 'http://localhost:4000/friendship/accept', res).then(() => {
+                console.log('Email sent');
+            }, (e: Error) => {
+                catchError(e, res);
+            });
+        } else if (status == 'Rejected') {
+            const deletedFriendship = await Friendship.findByIdAndDelete(id);
+            if (!deletedFriendship) {
+                return notFound(res, 'Friendship');
+            }
+            res.json({message: 'Friendship deleted'});
+
+            // Hit the notifier endpoint to send an email
+            await hitNotifier(user2._id.toString(), user1._id.toString(), 'http://localhost:4000/friendship/reject', res).then(() => {
+                console.log('Email sent');
+            }, (e: Error) => {
+                catchError(e, res);
+            });
         }
-
-        friendship.status = status;
-        await friendship.save();
-
-        res.json(friendship);
     } catch (e) {
         catchError(e, res);
     }
--- notifier/src/routes/friendship.ts
+++ notifier/src/routes/friendship.ts
@@ -4,7 +4,7 @@ import {sendEmail} from "../emailService";
 
 const friendshipRouter = express.Router();
 
-friendshipRouter.post('/', async (req, res) => {
+friendshipRouter.post('/request', async (req, res) => {
     const {userEmail, friend} = req.body;
     sendEmail('Friendship request', `User ${friend} wants to be friends with you`, userEmail).then(() => {
         res.json({message: 'Friendship request sent'});
@@ -15,4 +15,27 @@ friendshipRouter.post('/', async (req, res) => {
     });
 
 });
+
+friendshipRouter.post('/accept', async (req, res) => {
+    const {userEmail, friend} = req.body;
+    sendEmail('Friendship accepted', `User ${friend} has accepted your friend request`, userEmail).then(() => {
+        res.json({message: 'Friendship accepted'});
+
+    }).catch((e) => {
+        console.error(e);
+        res.status(500).json({message: 'An error occurred while sending the email'});
+    });
+});
+
+friendshipRouter.post('/reject', async (req, res) => {
+    const {userEmail, friend} = req.body;
+    sendEmail('Friendship rejected', `User ${friend} has rejected your friend request`, userEmail).then(() => {
+        res.json({message: 'Friendship rejected'});
+
+    }).catch((e) => {
+        console.error(e);
+        res.status(500).json({message: 'An error occurred while sending the email'});
+    });
+});
+
 export default friendshipRouter;
