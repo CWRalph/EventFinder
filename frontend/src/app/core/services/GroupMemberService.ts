3c3a25c12e1421ae623a725b094e651ff50dbf1f
TJ Grewal <tjgrewal.dev@gmail.com>
2024-04-12 09:34:56 -0700

    Update for Bobby

--- frontend/src/app/core/services/GroupMemberService.ts
+++ frontend/src/app/core/services/GroupMemberService.ts
@@ -11,7 +11,7 @@ export class GroupMemberService {
 
   private readonly LOCAL_URL = 'http://localhost:3000/group-memberships';
   private readonly PROD_URL = this.document.location.origin + '/group-memberships';
-  URL = this.LOCAL_URL;
+  URL = this.PROD_URL;
 
 
   getGroupMembers(): Observable<Group[]> {
